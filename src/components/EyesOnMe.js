ea7c08982427c860f49c07decb4a1b31187ecca4
Joseph_Thairu <joejoement@gmail.com>
2024-05-01 12:07:04 +0300

    completed

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+export default function EyesOnMe() {
+    function handleOnFocus(){
+        console.log("Good!");
+    }
+    function handleOnBlur(){
+        console.log("Hey! Eyes on me!");
+    }
+  return (
+    <div>
+        <button onFocus={handleOnFocus} onBlur={handleOnBlur} >Eyes on me</button>
+    </div>
+  )
+}
+
